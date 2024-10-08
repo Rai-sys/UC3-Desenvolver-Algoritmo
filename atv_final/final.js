7ae5d51d4254e86a570d9bb4bbaf66aeb399761c
Rai-sys <railaneserafim484@gmail.com>
2024-10-08 16:53:36 -0300

    add: codigo no arquivo js da pasta 'atv_final'

---
+++ atv_final/final.js
@@ -0,0 +1,98 @@
+//    Questão 01
+// let frutas = ["maçã", "banana", "laranja"];
+// frutas.push("manga");
+// frutas.unshift("uva");
+// frutas.splice(3,1);
+
+// console.log(frutas);
+
+//    Questão 02
+// let saudacao = "olá";
+// let nome = "maria";
+// let fala = `${saudacao}, ${nome}!`;
+
+// console.log(fala);
+
+//    Questão 03
+//???????????????????????????????????????????????????????
+
+//    Questão 04
+// let numeros = [5, 12, 8, 130, 44];
+// let maior10 = numeros.filter(function(num) {
+//     return num > 10;
+// });
+
+// console.log(maior10);
+
+//    Questão 05
+// let valores = [10, 20, 30, 40];
+// let soma = 0;
+
+// for (let i = 0;i < valores.length; i++){
+//    soma += valores[i];
+// };
+
+// console.log(soma);
+
+//    Questão 06
+// let texto = "javascript é legal";
+// let textoMaiusculo = texto.toUpperCase();
+
+// console.log(texto);
+// console.log(textoMaiusculo);
+
+//    Questão 07
+// let nome = "Carlos";
+// if (nome.indexOf('a')) {
+//     console.log(true);
+// }
+
+// console.log(nome);
+
+//    Questão 08
+// let cores = ["azul", "verde", "amarelo", "roxo"];
+// cores.splice(1,2)
+
+// console.log(cores);
+
+//    Questão 09
+// let numeros = [10, 20, 30];
+// numeros.unshift(5);
+// numeros.push(40);
+// numeros.pop();
+// numeros.shift();
+
+// console.log(numeros);
+
+//    Questão 10
+// let frase = "programação é divertida";
+
+// console.log(frase);
+// console.log(frase.length);
+
+//    Questão 11
+// let frase = "aprender javascript é muito bom";
+// let palavras = frase.split(" ");
+
+// console.log(palavras);
+
+//    Questão 12
+// let texto = "eu gosto de javascript";
+// let palavras = texto.split(" ").reverse().join(" ");
+
+// console.log(texto);
+// console.log(palavras);
+
+//    Questão 13
+// let numeros = [1, 2, 3, 4, 5];
+// let dobrado = numeros.map(function(num) {
+//     return num * 2;
+// });
+
+// console.log(dobrado);
+
+//    Questão 14
+
+//    Questão 15
+
+//    Questão 16
